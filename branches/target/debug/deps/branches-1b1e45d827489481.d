a284f2e3e85799667cc0a6c83afc3e27d24a56d8
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-12-12 15:00:47 +0100

    first commit

---
+++ branches/target/debug/deps/branches-1b1e45d827489481.d
@@ -0,0 +1,5 @@
+/home/oussama/projects/branches/target/debug/deps/branches-1b1e45d827489481.rmeta: src/main.rs
+
+/home/oussama/projects/branches/target/debug/deps/branches-1b1e45d827489481.d: src/main.rs
+
+src/main.rs:
---
+++ branches/target/debug/deps/branches-3eb221444c2d77ef.d
@@ -0,0 +1,5 @@
+/home/oussama/projects/branches/target/debug/deps/branches-3eb221444c2d77ef.rmeta: src/main.rs
+
+/home/oussama/projects/branches/target/debug/deps/branches-3eb221444c2d77ef.d: src/main.rs
+
+src/main.rs:
