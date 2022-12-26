621e7d9e49a4f161718c83c8c0991c466c004903
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-12-26 16:24:43 +0100

    compiles but runs infinitly

--- filter-less/src/main.rs
+++ filter-less/src/main.rs
@@ -12,17 +12,44 @@ fn main() {
         let mut blue = 0;
         let mut n = 0.0;
 
-        for i in x - 1..x + 1 {
-            for j in y - 1..y + 1 {
-                if i < img.get_width() && j < img.get_height() {
-                    red = img.get_pixel(i, j).r + red;
-                    green = img.get_pixel(i, j).g + green;
-                    blue = img.get_pixel(i, j).b + green;
-                    n = n + 1.0;
-                } else {
-                    continue;
+        // for i in x - 1..x + 1 as i64 {
+        //     for j in y - 1..y + 1 as i64 {
+        //         if i < img.get_width() && j < img.get_height() {
+        //             red = img.get_pixel(i, j).r + red;
+        //             green = img.get_pixel(i, j).g + green;
+        //             blue = img.get_pixel(i, j).b + green;
+        //             n = n + 1.0;
+        //         } else {
+        //             continue;
+        //         }
+        //     }
+        // }
+        let x2: i64 = x as i64;
+        let y2: i64 = y as i64;
+        let mut i: i64 = (x2 - 1) as i64;
+        loop {
+            if i <= (x + 1) as i64 {
+                let mut j: i64 = (y2 - 1) as i64;
+
+                loop {
+                    if j <= (y + 1) as i64 {
+                        if (i as u32) < img.get_width() && (j as u32) < img.get_height() {
+                            red = img.get_pixel(i as u32, j as u32).r + red;
+                            green = img.get_pixel(i as u32, j as u32).g + green;
+                            blue = img.get_pixel(i as u32, j as u32).b + green;
+                            n = n + 1.0;
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        break;
+                    }
+                    j += 1;
                 }
+            } else {
+                break;
             }
+            i += 1;
         }
         img.set_pixel(
             x,
